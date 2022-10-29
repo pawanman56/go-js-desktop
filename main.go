6ed946d1ec17fd7f3b707f1b4217bf8511745f34
Pawan Sunuwar <pawansunuwar@Pawan-3.local>
2022-10-30 00:04:24 +0545

    implement backend

--- main.go
+++ main.go
@@ -1,28 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"net/url"
+	"net"
+	"net/http"
+	"os"
+	"runtime"
 
 	"github.com/zserge/lorca"
 )
 
+func getUserName() string {
+	var envKey string
+
+	if runtime.GOOS == "windows" {
+		envKey = "USERNAME"
+	} else {
+		envKey = "USER"
+	}
+
+	return os.Getenv(envKey)
+}
+
 func main() {
 	// Pass HTML from data URI
-	ui, err := lorca.New("data:text/html,"+url.PathEscape(`
-		<html>
-			<head>
-			<title>Lorca App</title>
-			</head>
-			<body>
-				<h1 style="padding-top: 40vh; text-align: center;">Hello, Lorca!</h1>
-			</body>
-		</html>
-	`), "", 600, 400)
+	ui, err := lorca.New("", "", 600, 400)
+
+	ui.Bind("getUserName", getUserName)
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer ln.Close()
+
+	go http.Serve(ln, http.FileServer(http.Dir("./www")))
+	ui.Load(fmt.Sprintf("http://%s", ln.Addr()))
 
 	defer ui.Close()
 	<-ui.Done()
